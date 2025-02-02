30efc584c4a18eb4ddec4e8bf3bc105548a04d5b
Himanshu Krishna Yadav <himanshukrishnay12@gmail.com>
2025-02-02 16:41:29 +0545

    Added Animator

---
+++ Assets/Script/StaticStrings.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public static class StaticStrings
+{
+   public const string aniDeath = "Death";
+   public const string aniJump = "Jump";
+   public const string aniCombatMode = "InCombat";
+   public const string aniConsecutiveAttack = "ConsecutiveAttack";
+   public const string aniBlock="Block";
+   public const string aniAreaAttack="AreaAttack";
+   public const string aniMove="Move";
+   public const string aniMoveForward="MoveForward";
+   public const string aniRunForward = "RunForward";
+   
+}
